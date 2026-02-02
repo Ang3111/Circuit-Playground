2613fb4753c9f10378868dd1ece0d5398daf785c
Ang3111 <angelpsanchez23@gmail.com>
2026-02-02 09:07:54 -0800

    Create CPX-NeoPixel-N-LED-Blink.py

---
+++ CPX-NeoPixel-N-LED-Blink.py
@@ -0,0 +1,25 @@
+import time
+import board
+import neopixel
+
+while True:
+    pixels[0]=(255, 0, 0)
+    pixels.fill((255, 0, 0))
+    pixels[1]=(0, 255, 0)
+    pixels.fill((0, 255, 0))
+    pixels[2]=(0, 255, 0)
+    pixels.fill((0, 255, 0))
+    pixels[3]=(0, 255, 0)
+    pixels.fill((0, 255, 0))
+    pixels[4]=(0, 255, 0)
+    pixels.fill((0, 255, 0))
+    pixels[5]=(0, 255, 0)
+    pixels.fill((0, 255, 0))
+    pixels[6]=(0, 255, 0)
+    pixels.fill((0, 255, 0))
+    pixels[7]=(0, 255, 0)
+    pixels.fill((0, 255, 0))
+    pixels[8]=(0, 0, 255)
+    pixels.fill((0, 255, 0))
+    pixels[9]=(0, 255, 0)
+    pixels.fill((0, 255, 0))
